5efaff5bd6564b79f8f2c4771b58b687f1928276
Abdulmalik Abubakar <abumuhab98@gmail.com>
2024-12-12 18:49:57 +0100

    chapter 3: compacted logs

---
+++ c3_compacted_log/kvstore/wal.go
@@ -0,0 +1,357 @@
+package kvstore
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type WalEntryType int
+
+const (
+	WalEntryTypeSetCommand = iota
+	WalEntryTypeDeleteCommand
+)
+
+type walEntry struct {
+	Index     uint64       `json:"index"`
+	Data      []byte       `json:"data"`
+	EntryType WalEntryType `json:"entryType"`
+}
+
+func (entry *walEntry) keyValue() (*string, *string) {
+	if entry.EntryType == WalEntryTypeSetCommand {
+		command := SetValueCommand{}
+		command.fromWalEntry(*entry)
+		return &command.Key, &command.Value
+	} else if entry.EntryType == WalEntryTypeDeleteCommand {
+		command := DeleteValueCommand{}
+		command.fromWalEntry(*entry)
+		return &command.Key, nil
+	}
+	return nil, nil
+}
+
+type walMetadata struct {
+	SortedSegmentsMetadata []*walSegmentMetadata `json:"sortedSegmentsMetadata"`
+}
+
+func (meta *walMetadata) sortMetadata() {
+	sort.Slice(meta.SortedSegmentsMetadata, func(i, j int) bool {
+		if meta.SortedSegmentsMetadata[i].SegmentIndex != meta.SortedSegmentsMetadata[j].SegmentIndex {
+			return meta.SortedSegmentsMetadata[i].SegmentIndex < meta.SortedSegmentsMetadata[j].SegmentIndex
+		}
+		return meta.SortedSegmentsMetadata[i].CreatedAt.Before(meta.SortedSegmentsMetadata[j].CreatedAt)
+	})
+}
+
+type wal struct {
+	Entries              []walEntry
+	sortedSegments       []*walSegment
+	openSegment          *walSegment
+	metatada             *walMetadata
+	segmentCleanupTicker *time.Ticker
+	cleaningSegments     bool
+	segmentCleanupMutex  sync.Mutex
+}
+
+func newWal() *wal {
+	wal := &wal{}
+	wal.startCleanupTicker()
+	return wal
+}
+
+func (wal *wal) metaPath() string {
+	return filepath.Join("dat", "meta", "wal_metadata.dat")
+}
+
+func (wal *wal) newMetadata(segmentIndex uint64, firstEntryIndex uint64) *walSegmentMetadata {
+	metadata := &walSegmentMetadata{
+		SegmentIndex: segmentIndex,
+	}
+
+	metadata.FirstEntryIndex = firstEntryIndex
+	metadata.LastEntryIndex = firstEntryIndex
+	metadata.CreatedAt = time.Now()
+	metadata.Id = uuid.NewString()
+
+	return metadata
+}
+
+func (wal *wal) openNewSegment(index uint64, previousSegment *walSegment) *walSegment {
+	if wal.openSegment != nil {
+		wal.openSegment.close()
+	}
+
+	var firstEntryIndex uint64 = 0
+	if previousSegment != nil {
+		firstEntryIndex = previousSegment.meta.LastEntryIndex
+	}
+
+	meta := wal.newMetadata(index, firstEntryIndex)
+	segment := newWalSegment(meta)
+
+	wal.sortedSegments = append(wal.sortedSegments, segment)
+	wal.metatada.SortedSegmentsMetadata = append(wal.metatada.SortedSegmentsMetadata, meta)
+	wal.openSegment = segment
+
+	return segment
+}
+
+func (wal *wal) deleteSegment(segmentId string) {
+	//get the segment meta segmentMetadataIndex
+	segmentMetadataIndex := 0
+	var segmentToDelete *walSegment
+	for i, m := range wal.metatada.SortedSegmentsMetadata {
+		if m.Id == segmentId {
+			segmentMetadataIndex = i
+			segmentToDelete = newWalSegment(m)
+			break
+		}
+	}
+
+	//remove it from the metadata
+	sortedSegments := wal.metatada.SortedSegmentsMetadata
+	wal.metatada.SortedSegmentsMetadata = append(sortedSegments[:segmentMetadataIndex], sortedSegments[segmentMetadataIndex+1:]...)
+
+	//delete the segment file
+	segmentToDelete.meta.deleteSegmentLogFile()
+
+	//save the updated metadata without the meta of the deleted segment
+	wal.metatada.sortMetadata()
+	wal.saveMetadata()
+}
+
+func (wal *wal) getNextDirtySegment() *walSegmentMetadata {
+	wal.loadMetadata()
+	var value *walSegmentMetadata
+
+	for _, meta := range wal.metatada.SortedSegmentsMetadata {
+		if !meta.IsCompactedSegment {
+			value = meta
+			break
+		}
+	}
+
+	return value
+}
+
+func (wal *wal) saveMetadata() {
+	wal.metatada.sortMetadata()
+	data, err := json.Marshal(wal.metatada)
+	if err != nil {
+		panic(err)
+	}
+
+	os.MkdirAll("dat/meta", 0755)
+	os.WriteFile(wal.metaPath(), data, 0644)
+}
+
+func (wal *wal) loadMetadata() {
+	bytes, err := os.ReadFile(wal.metaPath())
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return
+		}
+		panic(err)
+	}
+
+	err = json.Unmarshal(bytes, &wal.metatada)
+	if err != nil {
+		panic(err)
+	}
+
+	wal.metatada.sortMetadata()
+}
+
+func (wal *wal) readSegments() {
+	wal.loadMetadata()
+	var segments []*walSegment = []*walSegment{}
+
+	for _, meta := range wal.metatada.SortedSegmentsMetadata {
+		segments = append(segments, newWalSegment(meta))
+	}
+
+	sort.Slice(segments, func(i, j int) bool {
+		return segments[i].meta.SegmentIndex < segments[j].meta.SegmentIndex
+	})
+
+	if len(segments) > 0 {
+		wal.openSegment = segments[len(segments)-1]
+		wal.sortedSegments = segments
+	} else {
+		wal.openNewSegment(0, nil)
+	}
+}
+
+func (wal *wal) maybeRoll() {
+	if wal.openSegment.meta.isAtCapacity() || wal.openSegment.meta.Closed {
+		wal.openNewSegment(wal.openSegment.meta.SegmentIndex+1, wal.openSegment)
+	}
+}
+
+func (wal *wal) WriteEntry(entry *walEntry) error {
+	wal.maybeRoll()
+	err := wal.openSegment.writeEntry(entry, nil)
+
+	if err == nil {
+		wal.saveMetadata()
+		wal.Entries = append(wal.Entries, *entry)
+	}
+	return err
+}
+
+func (wal *wal) ReadEntries() error {
+	wal.readSegments()
+
+	entries := []walEntry{}
+	for _, segment := range wal.sortedSegments {
+		if segment.meta.IsCompactedSegment && !segment.meta.CompactionCompleted {
+			continue
+		}
+
+		segmentEntries, err := segment.readEntries()
+		if err != nil {
+			return err
+		}
+
+		entries = append(entries, segmentEntries...)
+	}
+
+	wal.Entries = entries
+	return nil
+}
+
+func (wal *wal) startCleanupTicker() {
+	ticker := time.NewTicker(1 * time.Minute)
+
+	if wal.segmentCleanupTicker != nil {
+		wal.segmentCleanupTicker.Stop()
+	}
+
+	wal.segmentCleanupTicker = ticker
+
+	go func() {
+		for range ticker.C {
+			wal.cleanSegments()
+		}
+	}()
+}
+
+func (wal *wal) cleanSegments() {
+	if wal.cleaningSegments {
+		return
+	}
+	wal.segmentCleanupMutex.Lock()
+	wal.cleaningSegments = true
+
+	defer wal.segmentCleanupMutex.Unlock()
+	defer func() { wal.cleaningSegments = false }()
+
+	offsetMap := make(map[string]*uint64)
+	cleanedSegments := []*walSegment{}
+	segmentToClean := newWalSegment(wal.getNextDirtySegment())
+
+	if segmentToClean == nil || !segmentToClean.meta.Closed {
+		return
+	}
+
+	segmentToClean.processEntries(func(entry walEntry) {
+		key, _ := entry.keyValue()
+		if key == nil {
+			return
+		}
+
+		offset, exists := offsetMap[*key]
+		if !exists || entry.Index > *offset {
+			offsetMap[*key] = &entry.Index
+		}
+	})
+
+	entries := []walEntry{}
+
+	for _, meta := range wal.metatada.SortedSegmentsMetadata {
+		segment := newWalSegment(meta)
+
+		if segmentToClean.meta.SegmentIndex >= segment.meta.SegmentIndex && segment.meta.Closed {
+			segment.processEntries(func(entry walEntry) {
+				key, value := entry.keyValue()
+				if key == nil {
+					return
+				}
+
+				offset, exists := offsetMap[*key]
+				if !exists && value != nil {
+					entries = append(entries, entry)
+				} else if entry.Index >= *offset {
+					if value == nil {
+						tempEntries := []walEntry{}
+						for _, e := range entries {
+							k, _ := e.keyValue()
+							if key != k {
+								tempEntries = append(tempEntries, e)
+							}
+						}
+						entries = tempEntries
+					} else {
+						entries = append(entries, entry)
+					}
+				}
+			})
+
+			cleanedSegments = append(cleanedSegments, segment)
+		}
+
+		if segment.meta.Id == segmentToClean.meta.Id {
+			break
+		}
+	}
+
+	var newSegmentMetas []*walSegmentMetadata
+
+	batchSize := walSegmentSize
+	currentBatchSize := 0
+
+	var currentSegment *walSegment
+	var currentSegmentMeta *walSegmentMetadata
+
+	for index, entry := range entries {
+		if currentBatchSize == 0 {
+			currentSegmentMeta = wal.newMetadata(uint64(len(newSegmentMetas)), entry.Index)
+			currentSegmentMeta.IsCompactedSegment = true
+			currentSegmentMeta.CompactionCompleted = false
+			currentSegmentMeta.FirstEntryIndex = entry.Index
+			currentSegment = newWalSegment(currentSegmentMeta)
+			newSegmentMetas = append(newSegmentMetas, currentSegmentMeta)
+		}
+
+		currentSegment.writeEntry(&entry, &entry.Index)
+		currentBatchSize += 1
+
+		if currentBatchSize >= batchSize || index == len(entries)-1 {
+			currentSegmentMeta.LastEntryIndex = entry.Index
+
+			wal.metatada.SortedSegmentsMetadata = append(wal.metatada.SortedSegmentsMetadata, currentSegmentMeta)
+			wal.saveMetadata()
+
+			currentBatchSize = 0
+		}
+	}
+
+	for _, meta := range newSegmentMetas {
+		meta.CompactionCompleted = true
+		meta.Closed = true
+	}
+	wal.saveMetadata()
+
+	for _, segment := range cleanedSegments {
+		wal.deleteSegment(segment.meta.Id)
+	}
+	wal.saveMetadata()
+}
---
+++ c3_compacted_log/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"keyvault/kvstore"
+	"net/http"
+)
+
+var store *kvstore.KvStore = kvstore.NewKvStore()
+
+type PutRequest struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func (request *PutRequest) isValid() bool {
+	return request.Key != "" && request.Value != ""
+}
+
+func handleHttpError(w http.ResponseWriter, e error) {
+	if e == nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), 400)
+		return
+	}
+	http.Error(w, e.Error(), 400)
+}
+
+func httpHandler(w http.ResponseWriter, req *http.Request) {
+	method := req.Method
+
+	if method == http.MethodGet {
+		query := req.URL.Query()
+		key := query.Get("key")
+
+		if key == "" {
+			handleHttpError(w, nil)
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		encoder := json.NewEncoder(w)
+
+		value := store.Get(key)
+		encoder.Encode(map[string]string{
+			"key":   key,
+			"value": value,
+		})
+	}
+
+	if method == http.MethodPost {
+		body, _ := io.ReadAll(req.Body)
+
+		var request PutRequest
+		err := json.Unmarshal(body, &request)
+		if err != nil || !request.isValid() {
+			handleHttpError(w, err)
+			return
+		}
+
+		store.Put(request.Key, request.Value)
+	}
+
+	if method == http.MethodDelete {
+		query := req.URL.Query()
+		key := query.Get("key")
+
+		if key == "" {
+			handleHttpError(w, nil)
+			return
+		}
+
+		store.Delete(key)
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/", httpHandler)
+	http.ListenAndServe(":8090", nil)
+}
